1fb2cbad0874982df52f7c36e46c50bc215c3621
梅花三十三 <1596944197@qq.com>
2024-10-02 18:45:05 +0800

    fix: 双斜杠修复

--- build.ts
+++ build.ts
@@ -11,7 +11,7 @@ let command = "";
 if (OS.platform() === "win32") {
   command = `echo #!/usr/bin/env node | cat - dist/index.js > temp && sleep 1 && mv temp dist/index.js `;
 } else {
-  command = `sed -i '1s;^;#!/usr/bin/env node\n;' dist/index.js`;
+  command = "sed -i '1s;^;#!/usr/bin/env node\\n;' dist/index.js";
 }
 
 execSync(command);
