ac1012bd05afb63bc78f632563b2547f10872a81
梅花三十三 <1596944197@qq.com>
2024-10-02 18:33:41 +0800

    fix: Ubuntu中括号解析错误

--- build.ts
+++ build.ts
@@ -7,7 +7,7 @@ await Bun.build({
 });
 
 execSync(
-  `(echo #!/usr/bin/env node | cat - dist/index.js > temp) && sleep 1 && mv temp dist/index.js `,
+  `echo #!/usr/bin/env node | cat - dist/index.js > temp && sleep 1 && mv temp dist/index.js `,
 );
 
 console.log("Build complete");
