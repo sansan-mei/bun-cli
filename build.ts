ae4040529d377b33ee207f4d8f31e8a2cb658b66
MEI <1596944197@qq.com>
2025-03-19 11:37:03 +0800

    fix: 解决Mac中build错误问题

--- build.ts
+++ build.ts
@@ -10,6 +10,8 @@ await Bun.build({
 let command = "";
 if (OS.platform() === "win32") {
   command = `echo #!/usr/bin/env node | cat - dist/index.js > temp && sleep 1 && mv temp dist/index.js `;
+} else if (OS.platform() === "darwin") {
+  command = "sed -i '' '1s;^;#!/usr/bin/env node\\n;' dist/index.js";
 } else {
   command = "sed -i '1s;^;#!/usr/bin/env node\\n;' dist/index.js";
 }
